1d771941e53018aeba31f96481cbd334ea378191
ViniCarmo <vinicius.carmo1106@gmail.com>
2025-04-07 16:54:30 -0300

    updating data

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,50 +2,34 @@ package Application;
 
 import db.DB;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         Connection conn = null;
         PreparedStatement st = null;
+
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                            + "VALUES"
-                            + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+            st = conn.prepareStatement("UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
 
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
-
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
             int rowsAffected = st.executeUpdate();
 
-                if (rowsAffected > 0){
-                    ResultSet rs = st.getGeneratedKeys();
-                    while(rs.next()){
-                        int id = rs.getInt(1);
-                        System.out.println("Pronto! Id igual a " + id);
-                    }
-                }else {
-                    System.out.println("No rows affected");
-                }
-        } catch (SQLException e) {
+            System.out.println("Pronto! Rows Affected: " + rowsAffected);
+        }catch (SQLException e){
             e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } finally {
-            DB.closeConnection();
+        }finally {
+            DB.closeStatement(st);
             DB.closeConnection();
         }
-    }
 
-}
+    }
+}   
